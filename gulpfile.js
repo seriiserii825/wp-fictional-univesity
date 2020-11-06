bcc2ef0a0d60e8e6f2b6c71ebbea97e7ae2a4b8b
serii <seriiburduja@gmail.com>
2020-11-06 21:58:50 +0200

    parent page

---
+++ gulpfile.js
@@ -0,0 +1,139 @@
+"use strict";
+
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const sass = require('gulp-sass');
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require("gulp-plumber");
+const gcmq = require('gulp-group-css-media-queries');
+const wait = require('gulp-wait'), notify = require("gulp-notify");
+const browserSync = require("browser-sync");
+
+// let siteUrl = 'http://wc-estore.host1670806.hostland.pro/';
+// let siteDir = '../wc-estore/';
+
+// let siteUrl = 'http://gadda.cf/';
+// let siteDir = '../bs-gadda/';
+
+// let siteUrl = 'http://gadda.cf/';
+// let siteDir = '../vue/';
+
+// let siteDir = '../gsap/';
+// let siteDir = '../javascript-petricenco/';
+
+// let siteUrl = 'http://javascript-petricenco.host1670806.hostland.pro/';
+// let siteDir = '../javascript-petricenco/';
+
+let siteUrl = 'http://wp-fictional-univesity.host1670806.hostland.pro/';
+
+// const siteDir = '../bs-bunavestire/';
+// const siteUrl = 'http://wp-bunavestire.host1670806.hostland.pro/';
+
+//let siteDir = '../js-movies/';
+
+// let siteUrl = 'http://dev.ilcaffecheaspettavi.it/';
+// let siteDir = '../bs-alytaly/';
+
+// let siteUrl = 'http://bassoservizi.cf/';
+// let siteDir = '../bs-bassoservizi/';
+
+//let siteUrl = 'http://consorziostorm.cf/';
+//let siteDir = '../bs-consorzio/';
+
+// let siteUrl = 'https://myrewind.it/';
+// let siteDir = '../bs-rewind/';
+
+// let siteUrl = 'http://bertan.ml/';
+// let siteDir = '../bs-bertan/';
+
+//let siteUrl = 'http://proseccoborgoluce.cf//';
+//let siteDir = '../bs-proseccobordoluce/';
+
+let isDev = true;
+let webpackConfig = {
+	output: {
+		filename: "webpack.js"
+	},
+	watch: false,
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', {
+							debug: true,
+							corejs: 3,
+							useBuiltIns: "usage"
+						}]]
+					}
+				}
+			}
+		]
+	},
+	mode: isDev ? 'development' : 'production',
+	devtool: isDev ? 'eval-source-map' : 'none',
+};
+
+gulp.task('webpack', function () {
+	return gulp.src('assets/js/src/main.js')
+		.pipe(webpack(webpackConfig))
+		.pipe(gulp.dest('assets/js/dist/'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task("scss", function () {
+	return gulp.src('assets/scss/my.scss')
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(wait(500))
+		.pipe(sass({
+			outputStyle: 'expanded',
+			allowEmpty: true
+		}).on('error', notify.onError(function (error) {
+			return 'An error occurred while compiling sass.\nLook in the console for details.\n' + error;
+		})))
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(gcmq())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('assets/css/'))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task("watch", function () {
+	gulp.watch('assets/scss/**/*.scss', gulp.series('scss'));
+	gulp.watch('assets/js/src/**/*.js', gulp.series('webpack'));
+});
+
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		proxy: {
+			target: siteUrl,
+			ws: true
+		},
+		reloadDelay: 1500
+	});
+
+	// browserSync.init({
+	// 	server: {
+	// 		baseDir: siteDir
+	// 	},
+	// 	notify: true
+	// });
+	gulp.watch("**/*.html").on('change', browserSync.reload);
+	gulp.watch("**/*.php").on('change', browserSync.reload);
+	gulp.watch("**/*.css").on('change', browserSync.reload);
+	gulp.watch("**/*.js").on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('browser-sync'));
+// gulp.task('default', gulp.parallel('scss', 'watch', 'browser-sync'));
+// gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+// gulp.task('default', gulp.series('webpack', 'scss', gulp.parallel('watch', 'browser-sync')));
--- page.php
+++ page.php
@@ -2,9 +2,10 @@
 get_header();
 ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_template_directory_uri().'/assets//images/ocean.jpg'?>);"></div>
+        <div class="page-banner__bg-image"
+             style="background-image: url(<?php echo get_template_directory_uri() . '/assets//images/ocean.jpg' ?>);"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title();?></h1>
+            <h1 class="page-banner__title"><?php the_title(); ?></h1>
             <div class="page-banner__intro">
                 <p>Dont forget to change text</p>
             </div>
@@ -14,12 +15,23 @@ get_header();
     <main id="primary" class="site-main">
 		<?php while ( have_posts() ) : the_post(); ?>
             <div class="container container--narrow page-section">
-                <div class="metabox metabox--position-up metabox--with-home-link">
-                    <p>
-                        <a class="metabox__blog-home-link" href="#"> <i class="fa fa-home" aria-hidden="true"></i> Back to About Us </a>
-                        <span class="metabox__main">Our History</span>
-                    </p>
-                </div>
+				<?php
+				$page_id         = get_the_ID();
+				$page_parrent_id = wp_get_post_parent_id( $page_id );
+				?>
+				<?php if ( $page_parrent_id !== 0 ): ?>
+                    <div class="metabox metabox--position-up metabox--with-home-link">
+                        <p>
+                            <a class="metabox__blog-home-link" href="<?php echo get_page_link( $page_parrent_id ); ?>">
+                                <i class="fa fa-home" aria-hidden="true">
+                                </i>
+                                Back to
+								<?php echo get_the_title( $page_parrent_id ); ?>
+                            </a>
+                            <span class="metabox__main"><?php the_title(); ?></span>
+                        </p>
+                    </div>
+				<?php endif; ?>
 
                 <!--<div class="page-links">-->
                 <!--    <h2 class="page-links__title">-->
