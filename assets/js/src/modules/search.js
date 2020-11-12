e6e8d96503128a4eb78c9d8de41cc00e5649a725
serii <seriiburduja@gmail.com>
2020-11-12 21:54:24 +0200

    escape and enter for overlay

--- assets/js/src/modules/search.js
+++ assets/js/src/modules/search.js
@@ -7,15 +7,30 @@ class Search {
 		this.events();
 	}
 
-
 	// 2. Events
 	events() {
-		this.openBtn.addEventListener('click', () => {
-			this.searchOverlay.classList.add('search-overlay--active');
-		});
-		this.closeBtn.addEventListener('click', () => {
-			this.searchOverlay.classList.remove('search-overlay--active');
-		});
+		this.openBtn.addEventListener('click', this.openOverlay.bind(this));
+		this.closeBtn.addEventListener('click', this.closeOverlay.bind(this));
+		document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
+	}
+
+	keyPressDispatcher(event) {
+		if (event.key === 'Enter') {
+			this.openOverlay();
+		} else if (event.key === 'Escape') {
+			this.closeOverlay();
+		}
+	}
+
+	// 3.Methods
+	openOverlay() {
+		document.body.classList.add('body-no-scroll');
+		this.searchOverlay.classList.add('search-overlay--active');
+	}
+
+	closeOverlay() {
+		document.body.classList.remove('body-no-scroll');
+		this.searchOverlay.classList.remove('search-overlay--active');
 	}
 }
 export default Search;
