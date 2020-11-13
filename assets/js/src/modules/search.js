390b758ce1e6c516db651ce784cfabb1c073ac2a
serii <seriiburduja@gmail.com>
2020-11-13 11:32:44 +0200

    search

--- assets/js/src/modules/search.js
+++ assets/js/src/modules/search.js
@@ -4,8 +4,13 @@ class Search {
 		this.openBtn = document.querySelector('.search-trigger');
 		this.closeBtn = document.querySelector('.search-overlay__close');
 		this.searchOverlay = document.querySelector('.search-overlay');
+		this.searchField = document.querySelector('#search-term');
+		this.resaultsDiv = document.querySelector('#search-overlay__results');
 		this.events();
 		this.overlayIsVisible = false;
+		this.typingTimer = null;
+		this.isSpinnerVisible = false;
+		this.previeousValue = null;
 	}
 
 	// 2. Events
@@ -13,6 +18,28 @@ class Search {
 		this.openBtn.addEventListener('click', this.openOverlay.bind(this));
 		this.closeBtn.addEventListener('click', this.closeOverlay.bind(this));
 		document.addEventListener('keydown', this.keyPressDispatcher.bind(this));
+		this.searchField.addEventListener('keyup', this.typingLogic.bind(this));
+	}
+
+	typingLogic() {
+		if (this.searchField.value !== this.previeousValue) {
+			clearTimeout(this.typingTimer);
+			if (this.searchField.value) {
+				if (!this.isSpinnerVisible) {
+					this.resaultsDiv.innerHTML = '<div class="spinner-loader"></div>';
+					this.isSpinnerVisible = true;
+				}
+				this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+			} else {
+				this.resaultsDiv.innerHTML = '';
+			}
+		}
+		this.previeousValue = this.searchField.value;
+	}
+
+	getResults() {
+		this.resaultsDiv.innerHTML = 'Results here';
+		this.isSpinnerVisible = false;
 	}
 
 	keyPressDispatcher(event) {
