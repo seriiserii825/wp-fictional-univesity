7a73a104eb2432f29039caaf3d4456a7a5ba7fe5
serii <seriiburduja@gmail.com>
2020-11-21 11:29:44 +0200

    search.scss

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,5 +1,7 @@
-import Search from './modules/search';
+// import Search from './modules/search';
+import ExportSidebar from "./modules/search-sidebar";
 
 document.addEventListener('DOMContentLoaded', function () {
-	const search = new Search();
+	// const search = new Search();
+	const exportSidebar = new ExportSidebar();
 });
--- inc/bs-styles.php
+++ inc/bs-styles.php
@@ -7,6 +7,7 @@ function bs_fictional_scripts()
     wp_enqueue_style('bs-fictional-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('bs-fictional-font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('bs-fictional-style', get_stylesheet_uri(), array(), _S_VERSION);
+    wp_enqueue_style('bs-fictional-my', get_template_directory_uri() . '/assets/css/my.css');
 
     wp_enqueue_script('bs-fictional-webpack-js', get_template_directory_uri() . '/assets/js/dist/webpack.js', [], null, true);
 }
