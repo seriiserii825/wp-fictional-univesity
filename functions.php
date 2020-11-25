59de24e7c3ae4788ea5cbc16deb1c28a34dacf3c
serii <seriiburduja@gmail.com>
2020-11-25 22:23:52 +0200

    acf

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ if ( ! defined( '_S_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
 	define( '_S_VERSION', '1.0.0' );
 }
+require_once __DIR__ . '/inc/acf.php';
 require_once __DIR__ . '/inc/func.php';
 require_once __DIR__ . '/inc/bs-setup.php';
 require_once __DIR__ . '/inc/bs-styles.php';
