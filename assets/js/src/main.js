4b117597b1c2ec7021a6d1b9afac98ef0155a21c
serii <seriiburduja@gmail.com>
2020-12-13 12:55:23 +0200

    some

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,7 +1,8 @@
 // import Search from './modules/search';
 import ExportSidebar from "./modules/search-sidebar";
 
-document.addEventListener('DOMContentLoaded', function () {
-	// const search = new Search();
-	const exportSidebar = new ExportSidebar();
+document.addEventListener("DOMContentLoaded", function () {
+  // const search = new Search();
+  new ExportSidebar();
+  console.log("one");
 });
--- header.php
+++ header.php
@@ -1,43 +1,45 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<?php wp_head(); ?>
-</head>
-<body <?php body_class();?>>
-<header class="site-header">
-    <div class="container">
-        <h1 class="school-logo-text float-left">
-            <a href="<?php echo home_url();?>"><strong>Fictional</strong> University</a>
-        </h1>
-        <div class="js-search-trigger site-header__search-trigger"> <i class="fa fa-search" aria-hidden="true"></i></div>
-        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
-        <div class="site-header__menu group">
-            <nav class="main-navigation">
-                <?php wp_nav_menu( [
-                	'theme_location'  => 'menu-1',
-                	'menu'            => '',
-                	'container'       => '',
-                	'container_class' => '',
-                	'container_id'    => '',
-                	'menu_class'      => '',
-                	'menu_id'         => '',
-                	'echo'            => true,
-                	'fallback_cb'     => 'wp_page_menu',
-                	'before'          => '',
-                	'after'           => '',
-                	'link_before'     => '',
-                	'link_after'      => '',
-                	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                	'depth'           => 0,
-                	'walker'          => '',
-                ] ); ?>
 
-            </nav>
-            <span id="search-open" class="search-trigger js-search-trigger">
-                <i class="fa fa-search" aria-hidden="true"></i>
-            </span>
-        </div>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <header class="site-header">
+    <div class="container">
+      <h1 class="school-logo-text float-left">
+        <a href="<?php echo home_url(); ?>"><strong>Fictional</strong> University</a>
+      </h1>
+      <div class="js-search-trigger site-header__search-trigger"> <i class="fa fa-search" aria-hidden="true"></i></div>
+      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+      <div class="site-header__menu group">
+        <nav class="main-navigation">
+          <?php wp_nav_menu([
+            'theme_location'  => 'menu-1',
+            'menu'            => '',
+            'container'       => '',
+            'container_class' => '',
+            'container_id'    => '',
+            'menu_class'      => '',
+            'menu_id'         => '',
+            'echo'            => true,
+            'fallback_cb'     => 'wp_page_menu',
+            'before'          => '',
+            'after'           => '',
+            'link_before'     => '',
+            'link_after'      => '',
+            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'depth'           => 0,
+            'walker'          => '',
+          ]); ?>
+
+        </nav>
+        <span id="search-open" class="search-trigger js-search-trigger">
+          <i class="fa fa-search" aria-hidden="true"></i>
+        </span>
+      </div>
     </div>
-</header>
+  </header>
